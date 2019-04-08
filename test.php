73170474c61643ead2e1034cf0df771293507799
liucai <cailiu@e-seenet.com>
2019-04-08 17:33:07 +0800

    develop add test.php

---
+++ test.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * 测试文档注释
+ * @file test.php
+ * @brief 这是一个测试文件
+ * @author liucai 
+ * @date 2019-04-08
+ * @version 1.0.0
+ */
+
+/**
+ * 计算$a和$b的和
+ * @brief 加法计算
+ * @param  integer $a 参数a
+ * @param  integer $b 参数b
+ * @return integer 参数a和参数b的和
+ */
+function sum($a, $b)
+{
+	return $a + $b;
+}
+
+/*
+ R      CorseeDocsRelease
+ R      CrossCompiler
+ R      ReleaseDPT4
+ R W    TryAndGo
+ R W    font_resource
+ R W    ui_resource
+ */
